2b5edae0a825312ed73b01cdcabfea1cbb1ed527
racha <rachaafaf15@gmail.com>
2025-01-08 09:07:01 +0100

    a

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 
-// Load the protobuf
+// Protobuf
 const PROTO_PATH = './todo.proto';
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
     keepCase: true,
@@ -12,7 +12,6 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
 });
 const todoProto = grpc.loadPackageDefinition(packageDefinition).todo;
 
-// In-memory data
 let users = [];
 let projects = [];
 let todos = [];
@@ -20,24 +19,47 @@ let userId = 1;
 let projectId = 1;
 let todoId = 1;
 
+// Variables pour calcul des temps de traitement
+let totalProcessingTimeMs = 0;
+let requestCount = 0;
+
+function calculateAverageTime() {
+    return (totalProcessingTimeMs / requestCount).toFixed(2);
+}
+
 // Service implementation
 const todoService = {
     AddUser: (call, callback) => {
+        const start = process.hrtime();
         const { name } = call.request;
         const newUser = { id: userId++, name };
         users.push(newUser);
+        const [seconds, nanoseconds] = process.hrtime(start);
+
+        const elapsedTimeMs = seconds * 1e3 + nanoseconds / 1e6;
+        totalProcessingTimeMs += elapsedTimeMs;
+        requestCount++;
+
         callback(null, { user: newUser });
     },
     AddProject: (call, callback) => {
+        const start = process.hrtime();
         const { name, userId } = call.request;
         const user = users.find(u => u.id === userId);
         if (!user) return callback(new Error('User not found'));
 
         const newProject = { id: projectId++, name, userId };
         projects.push(newProject);
+        const [seconds, nanoseconds] = process.hrtime(start);
+
+        const elapsedTimeMs = seconds * 1e3 + nanoseconds / 1e6;
+        totalProcessingTimeMs += elapsedTimeMs;
+        requestCount++;
+
         callback(null, { project: newProject });
     },
     AddTodo: (call, callback) => {
+        const start = process.hrtime();
         const { title, userId, projectId } = call.request;
         const user = users.find(u => u.id === userId);
         const project = projects.find(p => p.id === projectId);
@@ -47,30 +69,30 @@ const todoService = {
 
         const newTodo = { id: todoId++, title, completed: false, userId, projectId };
         todos.push(newTodo);
+        const [seconds, nanoseconds] = process.hrtime(start);
+
+        const elapsedTimeMs = seconds * 1e3 + nanoseconds / 1e6;
+        totalProcessingTimeMs += elapsedTimeMs;
+        requestCount++;
+
         callback(null, { todo: newTodo });
     },
-    GetUsers: (call) => {
-        users.forEach(user => call.write(user));
-        call.end();
-    },
-    GetProjects: (call) => {
-        projects.forEach(project => call.write(project));
-        call.end();
-    },
-    GetTodos: (call) => {
-        todos.forEach(todo => call.write(todo));
-        call.end();
-    },
     GetTodosByUser: (call) => {
+        const start = process.hrtime();
         const { userId } = call.request;
         todos
             .filter(todo => todo.userId === userId)
             .forEach(todo => call.write(todo));
         call.end();
+        const [seconds, nanoseconds] = process.hrtime(start);
+
+        const elapsedTimeMs = seconds * 1e3 + nanoseconds / 1e6;
+        totalProcessingTimeMs += elapsedTimeMs;
+        requestCount++;
     },
 };
 
-// Start the server
+// Server setup
 const server = new grpc.Server();
 server.addService(todoProto.TodoService.service, todoService);
 
@@ -78,4 +100,12 @@ const PORT = '0.0.0.0:50051';
 server.bindAsync(PORT, grpc.ServerCredentials.createInsecure(), () => {
     console.log(`gRPC server running at ${PORT}`);
     server.start();
+
+    // Affichage toutes les 10 secondes des statistiques
+    setInterval(() => {
+        if (requestCount > 0) {
+            console.log(`Temps de traitement moyen (ms): ${calculateAverageTime()}`);
+            console.log(`Nombre total de requêtes: ${requestCount}`);
+        }
+    }, 10000);
 });
